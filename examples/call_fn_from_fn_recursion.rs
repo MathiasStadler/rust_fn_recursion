6165846360c69ee32d14cbc20d4fefd1b7251ec5
Mathias <email@example.com>
2024-06-27 13:15:19 +0200

    call_fn_from_fn_recursion works

--- examples/call_fn_from_fn_recursion.rs
+++ examples/call_fn_from_fn_recursion.rs
@@ -9,36 +9,90 @@
 use log::{debug, error, info};
 // use stdext::function_name;
 
+fn main() {
+    // env_logger::init();
+    init_logger();
 
-
-fn main(){
-    env_logger::init();
-    
     error!("error");
     info!("info");
     debug!("debug");
-    
+
     run();
 }
 
-fn run(){
+fn run() {
     info!("start run");
 
-    let max_recursion_steps = 1;
+    let mut max_recursion_steps = 10;
     let recursion = 0;
 
-    let recursion = recursion_converse(max_recursion_steps,recursion);
+    debug!("\t\t###");
+    debug!("\t\t###");
+    debug!("START CONVERSE RECURSION");
+    let recursion = recursion_converse(max_recursion_steps, recursion);
 
-    info!("recursion {}",recursion);
+    info!("recursion {}", recursion);
+
+    max_recursion_steps = 10;
+
+    debug!("\t\t###");
+    debug!("\t\t###");
+    debug!("START RECURSION ");
+    let recursion = real_recursion(max_recursion_steps);
+
+    info!("recursion {}", recursion);
 
     info!("stop run");
 }
 
-fn recursion_converse(_max_recursion_steps:i32,mut recursion:i32) -> i32{
-
+fn recursion_converse(max_recursion_steps: i32, mut recursion: i32) -> i32 {
+    debug!("recursion Nr. => {}", recursion);
+    debug!("max_recursion_steps => {}", max_recursion_steps);
     recursion = recursion + 1;
 
-    recursion 
+    if recursion < max_recursion_steps {
+        info!("call next recursion");
+        recursion_converse(max_recursion_steps, recursion);
+    }
+
+    recursion
+}
+
+fn real_recursion(start_recursion: i32) -> i32 {
+    // init
+    let mut recursion = start_recursion;
+    debug!("recursion Nr. => {}", recursion);
+    recursion = recursion - 1;
+
+    if recursion > 0 {
+        info!("call next recursion");
+        real_recursion(recursion);
+    }
+
+    recursion
+}
+
+fn init_logger() {
+    use std::io::Write;
+
+    env_logger::builder()
+        .format(|buf, record| {
+            let warn_style = buf.default_level_style(log::Level::Info);
+            let _timestamp = buf.timestamp();
+            writeln!(
+                buf,
+                // FROM HERE
+                // https://docs.rs/env_logger/latest/src/custom_format/custom_format.rs.html#35
+                "{}:{}  {warn_style}{}{warn_style:#} - {}",
+                // record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                // chrono::Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args(),
+            )
+        })
+        .init();
 }
 
 // RUST_LOG=debug cargo run --example call_fn_from_fn_recursion
